a986748edfa9e5e8da3eb84ed79f32d52ec4e22c
xndpxs <xndpxs@outlook.com>
2024-05-21 17:37:14 -0500

    Minor changes

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from PySide6 import QtWidgets
 from smbhelper_functions import SmbHelper
 
 app = QtWidgets.QApplication(sys.argv)
-window =SmbHelper(app)
+window = SmbHelper(app)
 window.show()
 
 app.exec()
-
