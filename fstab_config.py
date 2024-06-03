aeb0ce426d499bd054c8f0f94ec55de95493ed05
xndpxs <xndpxs@outlook.com>
2024-06-03 15:53:28 -0500

    Improved fstab functions. Now creating and restoring /etc/fstab through the fstab.bak file

--- fstab_config.py
+++ fstab_config.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+import subprocess
 import os
+import shutil
 
 
 class FstabConfig:
@@ -9,71 +11,93 @@ class FstabConfig:
 
     # METHODS
     def fstab_create_temp(self):
-        with open(f"{self.sf.fstab_location}", "r") as file:
-            fstab_data = file.read()
-        with open("fstab.bak", "w+") as file:
-            file.write(fstab_data)
-        self.sf.text_edit.append("Fstab temsfcreated...")
+        try:
+            shutil.copyfile("/etc/fstab", "/etc/fstab.bak")
+            self.sf.text_edit.append("Fstab backup created...")
+        except FileNotFoundError as e:
+            self.sf.text_edit.append("No /etc/fstab file found.")
+        except IOError as e:
+            self.sf.text_edit.append(f"Failed to create temporary backup of fstab: {e}")
 
     def fstab_delete_temp(self):
-        os.remove("fstab.bak")
-        self.sf.text_edit.append("Fstab temp deleted...")
-        self.sf.button_apply.setEnabled(True)
+        try:
+            Path("/etc/fstab.bak").unlink()
+            self.sf.text_edit.append("Fstab temp deleted...")
+            self.sf.button_apply.setEnabled(True)
+        except FileNotFoundError as e:
+            self.sf.text_edit.append("No backup found...")
+        except OSError as e:
+            self.sf.text_edit.append("Failed to delete backup of fstab: {e}")
 
     def fstab_modify(self):
-        self.sf.text_edit.append("Modifying fstab...")
-        self.fstab_create_temp()
-        with open(f"{self.sf.fstab_location}", "r") as f:
-            lines = f.readlines()
-            if any(line.strip() == self.sf.fstab_entry.strip() for line in lines):
-                self.sf.text_edit.append(
-                    f"A line for {self.sf.fstab_entry} already exists. New entry not added."
-                )
-            else:
-                self.sf.text_edit.append("Appending to fstab...")
-                with open(f"{self.sf.fstab_location}", "a") as file:
-                    file.write(self.sf.fstab_entry.strip())
+        try:
+            self.sf.text_edit.append("Modifying fstab...")
+            self.fstab_create_temp()
+
+            with open(self.sf.fstab_location, "r") as f:
+                lines = f.readlines()
+                if any(line.strip() == self.sf.fstab_entry.strip() for line in lines):
+                    self.sf.text_edit.append(
+                        f"A line for {self.sf.fstab_entry} already exists. New entry not added."
+                    )
+                else:
+                    self.sf.text_edit.append("Appending to fstab...")
+                    with open(self.sf.fstab_location, "a") as file:
+                        file.write(self.sf.fstab_entry.strip())
+        except FileNotFoundError:
+            self.sf.text_edit.append(f"Error: {self.sf.fstab_location} not found.")
+        except IOError as e:
+            self.sf.text_edit.append(f"IOError occurred while modifying fstab: {e}")
 
     def fstab_validation(self):
         self.sf.text_edit.append("Validating fstab...")
-        os.system("systemctl daemon-reload")
+
         try:
-            os.system("mount -a")
+            subprocess.run(["systemctl", "daemon-reload"], check=True)
+            subprocess.run(["mount", "-a"], check=True)
             self.sf.text_edit.append(
-                "Fstab validation successful, enjoy your mount :D...\n"
+                "Fstab validation successful, enjoy your mount :D..."
             )
-        except OSError as e:
+            return True
+
+        except subprocess.CalledProcessError as e:
             self.sf.text_edit.append("Fstab validation failed...")
-            self.sf.text_edit.append(f"{e}")
+            self.sf.text_edit.append(f"Error: {e}")
+            self.sf.text_edit.append("Restoring fstab...")
+            self.restore_fstab()
             self.sf.text_edit.append(
-                "Please not restart and manually verify /etc/fstab!"
+                "Please manually verify /etc/fstab before restart the computer"
             )
+            return False
 
     def restore_fstab(self):
-        self.sf.text_edit.append("Restoring fstab...")
-        with open(f"{self.sf.fstab_location}", "r") as f:
-            lines = f.readlines()
-        with open(f"{self.sf.fstab_location}", "w") as f:
-            for line in lines:
-                if not line.startswith(self.sf.fstab_location):
-                    f.write(line)
-            print("Removed entry from /etc/fstab.")
+        backup_path = Path(f"{self.sf.fstab_location}.bak")
+        fstab_path = Path(self.sf.fstab_location)
+
+        if not backup_path.exists():
+            self.sf.text_edit.append(f"Backup file {backup_path} not found.")
+            return
+
+        try:
+            shutil.copy(backup_path, fstab_path)
+            self.sf.text_edit.append("Restored the backup of fstab from fstab.bak")
+        except IOError as e:
+            self.sf.text_edit.append(f"Failed to restore the backup of fstab: {e}")
 
     def create_share_folder(self):
-        if Path(self.sf.samba_share).exists():
+        share_path = Path(self.sf.samba_share)
+
+        if share_path.exists():
             self.sf.text_edit.append("Folder for mount already found. Using it...")
         else:
-            Path(self.sf.samba_share).mkdir()
+            share_path.mkdir(parents=True, exist_ok=True)
             self.sf.text_edit.append("Folder created...")
+
             try:
-                os.chown(
-                    Path(self.sf.samba_share),
-                    self.sf.uid,
-                    self.sf.gid,
-                )
-                self.sf.text_edit.append("Folder permissions added...\n")
+                os.chown(share_path, self.sf.uid, self.sf.gid)
+                self.sf.text_edit.append("Folder permissions added...")
             except KeyError as e:
                 self.sf.text_edit.append(
-                    f"Error: User or group '{self.sf.samba_user}' not found.\n"
+                    f"Error: User or group '{self.sf.samba_user}' not found."
                 )
-                self.sf.text_edit.append(f"{e}")
+                self.sf.text_edit.append(f"Error:{e}")
